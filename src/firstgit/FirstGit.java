5e96e75ad62038632082452b3762c2fd04057b10
cschultz57 <cschultz57@L11108042787.wctc.msft>
2016-08-30 16:08:59 -0500

    Added hello world

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -11,7 +11,7 @@ public class FirstGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Hello World");
     }
     
 }
